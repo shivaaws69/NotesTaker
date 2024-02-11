3daf34163bb18e64dd7319cf053590778e509dbd
RAMESH KUMAR VERMA <120879499+RAMESHKUMARVERMAGITHUB@users.noreply.github.com>
2024-02-11 11:32:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,14 @@
-(cd backend && python main.py) & (cd frontend && yarn && yarn start)
+# (cd backend && python main.py) & (cd frontend && yarn && yarn start)
+
+
+#!/bin/bash
+
+# Start the backend
+cd /app/backend
+pip install -r requirements.txt
+python app.py &
+
+# Start the frontend
+cd /app/frontend
+yarn install
+yarn dev
